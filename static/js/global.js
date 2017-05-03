ccda2aa432b812035c2858129a563e8be0248604
Prev <prevdev@gmail.com>
2017-05-03 18:43:22 +0900

    Improve design on HomeView

---
+++ static/js/global.js
@@ -0,0 +1,14 @@
+/*
+window.WebFontConfig = {
+	custom: {
+		families: ['Spoqa Han Sans:100,300,400,500,700'],
+		urls: ['https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css']
+	},
+	timeout: 60000
+};
+(function(d) {
+	var wf = d.createElement('script'), s = d.scripts[0];
+	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+	s.parentNode.insertBefore(wf, s);
+})(document);
+*/
--- views/home.py
+++ views/home.py
@@ -1,8 +1,10 @@
 from jikji import render_template, register_view
 from models.people import People
+from models.event import Event
 
 @register_view
 def home() :
 	return render_template('home.html',
-		people= People._instances
+		people=People._instances,
+		events=Event._instances,
 	)
